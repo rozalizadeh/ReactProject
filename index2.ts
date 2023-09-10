3cad5476c32a748fc511bf7f7f67720b609e0723
roza <rozalizadeh@gmail.com>
2023-09-10 21:46:51 +0330

    third exrecise

---
+++ index2.ts
@@ -0,0 +1,12 @@
+let lightColor:string = "red";
+switch(lightColor){
+  case "red" :
+  console.log("green")
+  break;
+  case "yellow" :
+  console.log("red");
+  break;
+  case "green" :
+  console.log("yellow");
+  break;
+}
