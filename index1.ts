5b01be7fe4b890414b76be4fc4edd043d9ffe0ab
roza <rozalizadeh@gmail.com>
2023-09-10 21:40:47 +0330

    second exercise

---
+++ index1.ts
@@ -0,0 +1,21 @@
+const score:number =87;
+let grade:string = "A";
+ 
+if(score>=90){
+console.log(grade="A");
+}
+else if(score>=80 ){
+  console.log(grade="B");
+}
+else if(score>=70 ){
+  console.log(grade="C");
+}
+else if(score>=60 ){
+  console.log(grade="ِ");
+}
+else if(score>=50 ){
+  console.log(grade="E");
+}
+else {
+  console.log(grade="F")
+}
