18e64e8c8195a800024d68b54917e40081feb6db
roza <rozalizadeh@gmail.com>
2023-09-11 15:06:51 +0330

    forth exercise

---
+++ index3.ts
@@ -0,0 +1,26 @@
+let weekDay:number=4;
+switch(weekDay) {
+  case 1:
+    console.log("saturday");
+    break;
+  case 2:
+    console.log("sunday");
+    break;
+  case 3:
+    console.log("monday");
+    break;
+  case 4:
+    console.log("tuesday");
+    break;
+  case 5:
+    console.log("wednesday");
+    break;
+  case 6:
+    console.log("thursday");
+    break;
+  case 7:
+    console.log("friday");
+    break;
+  default:
+    break;
+}
